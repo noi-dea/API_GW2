144a9c56d464ad267bd552acbd3ffcfbcd3e9c4f
Dean Noiron <noirondean@gmail.com>
2025-03-03 17:39:41 +0100

    creation userModel

---
+++ src/models/userModel.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    avatar: {
+        type: String,
+        required: false,
+        default: "https://i.pinimg.com/474x/72/48/77/724877d7438cd53dbe791b52019c5fe3.jpg",
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    wishlist: {
+        type: {
+            Array : mongoose.Schema.Types.ObjectId,
+        },
+        ref: "Product",
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
