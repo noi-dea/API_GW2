6c8aa50e2c0b965bd7f98acdd37af447c6c16881
Dean Noiron <noirondean@gmail.com>
2025-03-15 23:06:00 +0100

    added transaction post route to secure setterRoutes file

--- src/routes/setterRoutes.ts
+++ src/routes/setterRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addProduct } from "../controllers/productController";
 import { addUser } from "../controllers/userController";
+import { addTransaction } from "../controllers/transactionController";
 
 const router = Router();
 
-router.post("/products", addProduct).post("/users", addUser);
+router.post("/products", addProduct).post("/users", addUser).post("/transactions", addTransaction);
 
 export default router;
